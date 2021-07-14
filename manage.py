0b9c89c36fba1bc4bc0ccde908011d51d74cbb2b
Neemias Santos <santos.neemias@gmail.com>
2021-07-14 15:25:05 -0300

    Revert "- Changing project to conect with database;"
    This reverts commit 664b5506722a385925716c59a854117205588a00.

--- manage.py
+++ manage.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == '__main__':
-    # If WEBSITE_HOSTNAME is defined as an environment variable, then we're running
-    # on Azure App Service and should use the production settings.
-    settings_module = "azuresite.production" if 'WEBSITE_HOSTNAME' in os.environ else 'azuresite.settings'
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
+def main():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_hello.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -16,5 +14,8 @@ if __name__ == '__main__':
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
